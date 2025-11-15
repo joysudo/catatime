4c31fbecedd6e78501f470583a406e06797a47a3
Joy <joysu2008@gmail.com>
2025-11-15 18:15:36 -0500

    add color selector to settings

--- src/renderer/src/components/Settings.jsx
+++ src/renderer/src/components/Settings.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 
-function Settings({toggleSettings, maxHours, setMaxHours, username, setUsername}) {
+function Settings({toggleSettings, maxHours, setMaxHours, username, setUsername, themeColor, setThemeColor}) {
   const [tempMaxHours, setTempMaxHours] = useState(maxHours);
   const [tempUsername, setTempUsername] = useState(username);
+  const themeColors = ['LightPink', '#fcd95b', 'DarkSeaGreen', 'SkyBlue', 'DarkGray', '#eea9f5']
 
   const handleSave = () => {
     setMaxHours(Number(tempMaxHours));
     setUsername(tempUsername);
+  }
+  const handleColorChange = (color) => {
+    setThemeColor(color);
   }
 
   return (
@@ -29,6 +33,20 @@ function Settings({toggleSettings, maxHours, setMaxHours, username, setUsername}
           onChange={(e) => setTempUsername(e.target.value)}
         />
       </label>
+      <p>Theme color: </p>
+      <div className="color-circle-container">
+        {themeColors.map((color) => (
+          <div
+            key={color}
+            className={`color-circle`}
+            style={{
+              backgroundColor: color,
+              border: themeColor == color ? '3px solid white' : 'none'
+            }}
+            onClick={() => handleColorChange(color)}
+          ></div>
+        ))}
+      </div>
       <button onClick={handleSave}>Save</button>
       <button onClick={toggleSettings}>close settings</button>
     </div>
--- src/renderer/src/pages/CatCup.jsx
+++ src/renderer/src/pages/CatCup.jsx
@@ -4,26 +4,27 @@ import Settings from '../components/Settings';
 import { useState } from 'react'
 
 function CatCup() {
-  const [ownedItems, setOwnedItems] = useState([]);
-  const [hours, setHours] = useState(0);
-  const [currency, setCurrency] = useState(0);
+  const [ownedItems, setOwnedItems]=useState([]);
+  const [hours, setHours]=useState(0);
+  const [currency, setCurrency]=useState(0);
   // tabs
-  const [storeOpen, setStoreOpen] = useState(false);
-  const [inventoryOpen, setInventoryOpen] = useState(false);
-  const [settingsOpen, setSettingsOpen] = useState(false);
+  const [storeOpen, setStoreOpen]=useState(false);
+  const [inventoryOpen, setInventoryOpen]=useState(false);
+  const [settingsOpen, setSettingsOpen]=useState(false);
   // settings
-  const [maxHours, setMaxHours] = useState(3);
-  const fillPercent = hours/maxHours*100;
-  const [username, setUsername] = useState("user");
+  const [maxHours, setMaxHours]=useState(3);
+  const fillPercent=hours/maxHours*100;
+  const [username, setUsername]=useState("user");
+  const [themeColor, setThemeColor]=useState('pink');
 
-  const addHour = () => { // placeholder
+  const addHour=() => { // placeholder
     setHours(prev => Math.min(prev + 0.5, maxHours));
   };
-  const cashIn = () => {
+  const cashIn=() => {
     setCurrency(prev => prev + hours);
     setHours(0);
   };
-  const buyItem = (item) => { // has to be here bc currency is here
+  const buyItem=(item) => { // has to be here bc currency is here
     if (currency >= item.cost) {
       setCurrency(prev => prev - item.cost);
       setOwnedItems(prev => [...prev, item]);
@@ -32,41 +33,52 @@ function CatCup() {
       alert("no, you're poor");
     }
   };
-  const toggleStore = () => {
+  const toggleStore=() => {
     setStoreOpen(prev => !prev);
   };
-  const toggleInventory = () => {
+  const toggleInventory=() => {
     setInventoryOpen(prev => !prev);
   };
-  const toggleSettings = () => {
+  const toggleSettings=() => {
     setSettingsOpen(prev => !prev);
   };
 
   return (
-    <div className="cup-page-container">
-      <div className="cup">
-        <div className="cup-fill" style={{height: `${fillPercent}%`}}></div>
+    <div className="background" style={{backgroundColor: themeColor}}>
+      <div className="cup-page-container">
+        <div className="cup">
+          <div
+            className="cup-fill"
+            style={{
+              height: `${fillPercent}%`,
+              backgroundColor: themeColor,
+              filter: 'brightness(80%) saturate(250%)'
+            }}
+          ></div>
+        </div>
+        <div className="cup-info-container">
+          <h1>Welcome back, {username}!</h1> {/*randomizing this message later would be fun*/}
+          <p>Your cup is currently at {hours} hours out of the {maxHours} needed to fill.</p>
+          <p>you have {currency} coins to spend on cat-name. ready for a shopping spree?</p>
+          <button onClick={addHour} disabled={hours>=maxHours}>increase hours (placeholder)</button>
+          <button onClick={cashIn} disabled={hours < maxHours}>pour ur cup</button>
+          <div className="cup-button-container">
+            <button onClick={toggleStore}>store</button>
+            <button onClick={toggleInventory}>inventory</button>
+            <button onClick={toggleSettings}>settings</button>
+          </div>
+          {storeOpen && <Store currency={currency} buyItem={buyItem} toggleStore={toggleStore} ownedItems={ownedItems}/>}
+          {inventoryOpen && <Inventory ownedItems={ownedItems} toggleInventory={toggleInventory}/>}
+          {settingsOpen && <Settings toggleSettings={toggleSettings}
+            maxHours={maxHours}
+            setMaxHours={setMaxHours}
+            username={username}
+            setUsername={setUsername}
+            themeColor={themeColor}
+            setThemeColor={setThemeColor}
+          />}
+          </div>
       </div>
-      <div className="cup-info-container">
-        <h1>Welcome back, {username}!</h1> {/*randomizing this message later would be fun*/}
-        <p>your cup is currently at {hours} hours out of the {maxHours} needed to fill.</p>
-        <p>you have {currency} coins to spend on cat-name. ready for a shopping spree?</p>
-        <button onClick = {addHour} disabled={hours>=maxHours}>increase hours (placeholder)</button>
-        <button onClick = {cashIn} disabled={hours < maxHours}>pour ur cup</button>
-        <div className="cup-button-container">
-          <button onClick = {toggleStore}>store</button>
-          <button onClick = {toggleInventory}>inventory</button>
-          <button onClick = {toggleSettings}>settings</button>
-        </div>
-        {storeOpen && <Store currency={currency} buyItem={buyItem} toggleStore={toggleStore} ownedItems={ownedItems}/>}
-        {inventoryOpen && <Inventory ownedItems={ownedItems} toggleInventory={toggleInventory}/>}
-        {settingsOpen && <Settings toggleSettings={toggleSettings}
-          maxHours={maxHours}
-          setMaxHours={setMaxHours}
-          username={username}
-          setUsername={setUsername}
-        />}
-        </div>
     </div>
   )
 }
