73b1b92c7841f0a68e4e9a1360d1b47126ba0b70
Joy <joysu2008@gmail.com>
2025-11-16 02:35:50 -0500

    add accessories functionality

--- frontend/src/renderer/src/components/Store.jsx
+++ frontend/src/renderer/src/components/Store.jsx
@@ -1,12 +1,13 @@
+import coinImage from '../assets/images/coin.png';
+
 function Store({currency, buyItem, toggleStore, ownedItems}) {
   const inventory = [
-    {id: 1, name: "Cherry hat", cost: 5, src: "../assets/images/store/cherry-hat.png"},
-    {id: 2, name: "Paper umbrella", cost: 3, src: "../assets/images/store/paper-umbrella.png"},
-    {id: 3, name: "Striped straw", cost: 1, src: "../assets/images/store/striped-straw.png"},
-    {id: 4, name: "Basic straw", cost: 2, src: "../assets/images/store/basic-straw.png"},
-    {id: 5, name: "Midnight bird", cost: 6, src: "../assets/images/store/midnight-bird.png"}
+    {id: 1, name: "Cherry hat", cost: 5, src: "/src/assets/images/store/cherry-hat.png"},
+    {id: 2, name: "Mini umbrella", cost: 3, src: "/src/assets/images/store/mini-umbrella.png"},
+    {id: 3, name: "Choco straw", cost: 1, src: "/src/assets/images/store/choco-straw.png"},
+    {id: 4, name: "Saiki K", cost: 2, src: "/src/assets/images/store/saiki-k.png"},
+    {id: 5, name: "Midnight bird", cost: 6, src: "/src/assets/images/store/midnight-bird.png"}
   ]
-
 
   return (
     <div className="store">
@@ -15,7 +16,9 @@ function Store({currency, buyItem, toggleStore, ownedItems}) {
         {inventory.map(item => (
           <div key={item.id} className="store-item">
             <p>{item.name}</p>
-            <p><i>{item.cost} coins</i></p>
+            <p><i>{item.cost}&nbsp;
+              <img src={coinImage} style={{height: '16px', transform: 'translateY(20%)'}}/>
+            s</i></p>
             {!ownedItems.some(ownedItem => ownedItem.id === item.id) && currency >= item.cost &&
               <button className="glass glass-active" onClick={() => buyItem(item)} disabled={currency < item.cost}>buy</button>
             }
